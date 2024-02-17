1d7509b1346ae785b474b75bc2b083feb8dd6279
DEV-COM <kwp@tlen.pl>
2024-02-17 11:48:48 +0100

    11:48 17

--- zaj/src/functionBased/SearchInput.jsx
+++ zaj/src/functionBased/SearchInput.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 export default function SearchInput() {
+
+    const [filterText,setFilterText] = useState('');
+
+    const handleChange = (e) => {
+        setFilterText(e.target.value)
+    }
     return (
         <form>
-            <input type="text" value="Static text" placeholder="Search..." />
+            <input type="text" value={filterText} placeholder="Search..." 
+            onChange={handleChange}/>
         </form>
     )
 }
