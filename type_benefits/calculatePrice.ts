4efd9e397419fb2a98a304a9359655e268ddc84b
DEV-COM <kwp@tlen.pl>
2024-02-17 15:36:41 +0100

    15:36 17

--- type_benefits/calculatePrice.ts
+++ type_benefits/calculatePrice.ts
@@ -19,5 +19,41 @@ const tabela : {name: string, unitPrice?: number} = {
     name: "Table",
 }
 
+type Sprzedaz = (quantity: number) => void
+type Product = {name: string, unitPrice?:number, purchase?: Sprzedaz}
+
+interface Product2 {
+    name: string,
+    unitPrice?: number
+}
+
+class Product3 {
+    name = ""
+    unitPrice = 0
+}
+
+interface DiscountProduct2 extends Product2 {
+    rabat: number
+}
+
+interface Sprzedaz2 {(quantity: number): void}
+
+let chair: Product = {name: "Chair", purchase: (quantity) =>
+    console.log(`Zakupiono ${quantity} krzeseł`)
+}
 
 
+
+type DiscountProduct = Product & { rabat: number}
+
+let chairOnSale: DiscountProduct = {name: "Krzesło", unitPrice: 30,rabat: 5}
+
+
+enum Level {
+    Low,
+    Medium,
+    High
+}
+
+let level = Level.Low
+console.log(level)
