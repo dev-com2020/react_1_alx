8bfc0df08a93f4e7da89c8c3f0628fe0470a5648
DEV-COM <kwp@tlen.pl>
2024-02-17 10:26:57 +0100

    10:26 17

---
+++ zaj/src/classBased/ActivityCategory.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ActivityCategory extends Component {
+    render(){
+        return (
+            <tr>
+            <td>
+                ### Kategoria ###
+            </td>
+            </tr>
+        )
+    }
+}
