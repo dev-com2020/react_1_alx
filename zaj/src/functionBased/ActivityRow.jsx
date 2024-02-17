4a6e1f893ffc319c407e8fa4bad053a9ef4d8d9f
DEV-COM <kwp@tlen.pl>
2024-02-17 12:00:42 +0100

    12:00 17

--- zaj/src/functionBased/ActivityRow.jsx
+++ zaj/src/functionBased/ActivityRow.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react"
+
 export default function ActivityRow({ activity }) {
+    const [showDetails, toogleShowDetails] = useState(false)
     return (
         <tr>
             <td>{activity.name}</td>
-            <td> {activity.duration} h</td>
-            <td> {activity.done ? 'Done': 'Not started'} </td>
+            {
+                showDetails ?
+                    <>
+                        <td> {activity.duration} h</td>
+                        <td> {activity.done ? 'Done' : 'Not started'} </td>
+                    </> :
+                    <>
+                        <td>?</td>
+                        <td>?</td>
+                    </>
+            }
             <td>
-                <button>Show Details</button>
+                <button onClick={() => toogleShowDetails(!showDetails)}>
+                    {showDetails ? 'Hide': 'Show'}
+                    </button>
             </td>
         </tr>
     )
