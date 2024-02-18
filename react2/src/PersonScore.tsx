b5249cc900e2c1473a0e2ec4edf07571a249cd8c
DEV-COM <kwp@tlen.pl>
2024-02-18 10:26:47 +0100

    10:26 18

--- react2/src/PersonScore.tsx
+++ react2/src/PersonScore.tsx
@@ -1,18 +1,58 @@
-import { useEffect, useState } from "react"
+import { useEffect, useReducer, useState } from "react"
 import { getPerson } from "./getPerson"
+import { act } from "react-dom/test-utils"
 
-export function PersonScore(){
-    const [name,setName] = useState<string | undefined>()
-    const [score, setScore] = useState(0)
-    const [loading,setLoading] = useState(true)
+type State = {
+    name: string | undefined
+    score: number
+    loading: boolean
+}
+
+type Action =
+    | {
+        type: 'initialize'
+        name: string
+    }
+    | {
+        type: 'incement'
+    }
+    | {
+        type: 'decrement'
+    }
+    | {
+        type: 'reset'
+    }
+
+function reducer(state: State, action: Action): State {
+    switch (action.type) {
+        case 'initialize':
+            return { name: action.name, score: 0, loading: false }
+        case 'incement':
+            return { ...state, score: state.score + 1 }
+        case 'decrement':
+            return { ...state, score: state.score - 1 }
+        case 'reset':
+            return { ...state, score: 0 }
+        default:
+            return state
+    }
+}
+
+export function PersonScore() {
+    const [{ name, score, loading }, dispatch] = useReducer(
+        reducer,
+        {
+            name: undefined,
+            score: 0,
+            loading: true
+        }
+    )
     useEffect(() => {
-       getPerson().then((person) => {
-        setLoading(false)
-        setName(person.name)
-        // console.log("State values", loading, name)
-       })
+        getPerson().then(({ name }) =>
+            dispatch({ type: 'initialize', name })
+        )
     }, [])
-    if (loading){
+    if (loading) {
         return <div>Loading...</div>
     }
     return (
@@ -20,9 +60,9 @@ export function PersonScore(){
             <h3>
                 {name}, {score}
             </h3>
-            <button onClick={()=>setScore(score + 1)}>Add</button>
-            <button onClick={()=>setScore(score - 1)}>Substract</button>
-            <button onClick={()=>setScore(0)}>Reset</button>
+            <button onClick={() => dispatch({ type: 'incement' })}>Add</button>
+            <button onClick={() => dispatch({ type: 'decrement' })}>Substract</button>
+            <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
         </div>
     )
 }
