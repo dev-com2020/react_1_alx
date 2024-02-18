7866867d152e3cbe7c45f648ca337bfda7a64e28
DEV-COM <kwp@tlen.pl>
2024-02-18 10:09:01 +0100

    10:08 18

--- react2/src/PersonScore.tsx
+++ react2/src/PersonScore.tsx
@@ -1,12 +1,28 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getPerson } from "./getPerson"
 
 export function PersonScore(){
+    const [name,setName] = useState<string | undefined>()
+    const [score, setScore] = useState(0)
+    const [loading,setLoading] = useState(true)
     useEffect(() => {
-        async function getThePerson() {
-        const person = await getPerson()
-        console.log(person)
-        }
-        getThePerson()
+       getPerson().then((person) => {
+        setLoading(false)
+        setName(person.name)
+        // console.log("State values", loading, name)
+       })
     }, [])
+    if (loading){
+        return <div>Loading...</div>
+    }
+    return (
+        <div>
+            <h3>
+                {name}, {score}
+            </h3>
+            <button onClick={()=>setScore(score + 1)}>Add</button>
+            <button onClick={()=>setScore(score - 1)}>Substract</button>
+            <button onClick={()=>setScore(0)}>Reset</button>
+        </div>
+    )
 }
