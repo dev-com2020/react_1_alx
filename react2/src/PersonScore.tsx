943d584e6abdc3b6dbbf82774f089f785589d282
DEV-COM <kwp@tlen.pl>
2024-02-18 09:49:55 +0100

    09:49 18

---
+++ react2/src/PersonScore.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react"
+import { getPerson } from "./getPerson"
+
+export function PersonScore(){
+    useEffect(() => {
+        async function getThePerson() {
+        const person = await getPerson()
+        console.log(person)
+        }
+        getThePerson()
+    }, [])
+}
