c4276fde596d09a41f277be7043a03c67181c5f6
DEV-COM <kwp@tlen.pl>
2024-02-18 14:40:02 +0100

    14:39 18

--- react2/src/Routex.tsx
+++ react2/src/Routex.tsx
@@ -1,8 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ProductsPage } from "./ProductsPage";
+import { Header } from "./Headers";
 
 const router = createBrowserRouter([
     {
+        path: '/',
+        element: <Header />
+    }
+    ,{
         path: 'products',
         element: <ProductsPage />
     }
